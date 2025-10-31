c005068f6ec96c68953ae9d1c069651ef2c6d076
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-10-31 18:44:30 +0500

    Refactor typeCoercion.js to improve code readability and streamline leap year check logic

--- JavaScript Practice/JS Operators & Expressions/typeCoercion.js
+++ JavaScript Practice/JS Operators & Expressions/typeCoercion.js
@@ -7,7 +7,6 @@ console.log(false + 5); // 5 (false is coerced to number 0)
 console.log(10 == "10"); // true (string "10" is coerced to number 10)
 console.log("10" === 10); // false (no coercion, different types)
 
-
 //* Problem 1 — Output Prediction
 //* Predict the output and explain:
 console.log(5 + "5"); //* "55" (number 5 is coerced to string "5")
@@ -19,16 +18,18 @@ console.log(10 === "10"); //* false (no coercion, different types)
 //* Problem 2 — DSA Logic
 //* Write a program that checks whether a given year is a leap year.
 let year = 2023;
-if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
   console.log(year + " is a leap year.");
-}
-else {
-    console.log(year + " is not a leap year")
+} else {
+  console.log(year + " is not a leap year");
 }
 
 //* Other Way to Write Leap Year Check.
-console.log(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0 ? "Leap year" : "Not leap year");
-
+console.log(
+  (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+    ? "Leap year"
+    : "Not leap year"
+);
 
 //* 💡 MAANG tip: They love asking coercion trick questions like:
 console.log([] + []); // ? // ""
