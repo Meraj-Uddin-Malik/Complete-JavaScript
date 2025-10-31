0f662e75372649f4a6bf8fa26ea0d8670bd3e2fb
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-10-31 15:41:45 +0500

    Add variable declarations and console log to variable.js

---
+++ JavaScript Practice/JS Variables/variable.js
@@ -0,0 +1,9 @@
+// ! A variable is like a container that stores data (like numbers, text, or arrays).
+// ! In JavaScript, we use 3 keywords to declare variables:
+
+var x = 10;
+let y = 20;
+const z= 30;
+console.log(x, y, z);
+
+// ?Note: Always prefer let and const — var is outdated and risky due to hoisting issues.
