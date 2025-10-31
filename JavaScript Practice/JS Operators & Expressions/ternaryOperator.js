c594de4c757a2f19aa3f3d223a6f43cccc01b018
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-10-31 20:59:27 +0500

    Refactor ternary operator examples for clarity; fix console log for largest number calculation

--- JavaScript Practice/JS Operators & Expressions/ternaryOperator.js
+++ JavaScript Practice/JS Operators & Expressions/ternaryOperator.js
@@ -3,11 +3,19 @@ console.log(`
     It’s called ternary because it takes three operands:`);
 
 let age = 18;
-let canVote = (age >= 18) ? "Yes" : "No";
+let canVote = age >= 18 ? "Yes" : "No";
 console.log(`Can the person vote? ${canVote}`);
-
 
 //* DSA Challenge
 // Write a program that finds the largest of 3 numbers using only ternary operators.
-let a = 10, b = 25, c = 5;
-console.log(Math.max(a) + "The Largest number is " + Math.max(b) + " " + Math.max(c));
+let a = 10,
+  b = 25,
+  c = 5;
+let largest = a >= b ? (a >= c ? a : c) : b >= c ? b : c;
+console.log(`The Largest Num Is: ${largest}`);
+
+
+//* Interview Concept Check
+//* What is the difference between switch and if-else?
+//* Why do we use break in a switch statement?
+//* Can a switch check ranges (like case x > 5:)? Why or why not?
