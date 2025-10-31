7e9f2a6186debb0979ef17972938538956e4a96a
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-10-31 16:36:48 +0500

    Add interactive number comparison functionality to variable.js

--- JavaScript Practice/JS Variables/variable.js
+++ JavaScript Practice/JS Variables/variable.js
@@ -1,3 +1,5 @@
+const readline = require("readline");
+
 // ! A variable is like a container that stores data (like numbers, text, or arrays).
 // ! In JavaScript, we use 3 keywords to declare variables:
 
@@ -16,3 +18,33 @@
 // let favProgrammingLanguage = "JavaScript";
 
 // console.log(`Hi I am ${name} from ${country}, and I love coding in ${favProgrammingLanguage}.`);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter First Number: ", (num1) => {
+  rl.question("Enter Second Number: ", (num2) => {
+    rl.question("Enter Third Number: ", (num3) => {
+        //* Convert String Input To Num
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        num3 = parseFloat(num3);
+
+        //* Find Largest One
+        let largest;
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else if (num3 >= num1 && num3 >= num2) {
+            largest = num3;
+        }
+
+        console.log(`The largest number is: ${largest}`);
+
+      rl.close(); // Close the input interface
+    });
+  });
+});
