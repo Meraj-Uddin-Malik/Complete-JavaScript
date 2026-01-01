e62fc5e64a2f0a9c59289cd2f069229ee49046c9
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 01:26:05 +0500

    Add ID to span element and log innerHTML/outerHTML of the span

--- JavaScript Practice/JS innerHTML outerHTML/script.js
+++ JavaScript Practice/JS innerHTML outerHTML/script.js
@@ -6,3 +6,6 @@ console.log(x); // Hy I am a Span
 
 let y = document.getElementsByTagName("span")[0].outerHTML; 
 console.log(y); // <span>Hy I am a Span</span>
+console.log(first.innerHTML);
+console.log(first.outerHTML);
+console.log(document.body.firstChild.nodeValue)
