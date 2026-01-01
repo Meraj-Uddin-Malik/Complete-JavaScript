da14e33d6fa97a5694e0113fc7720ffe50cb20c7
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 01:27:03 +0500

    Refactor script.js for consistency and clarity in logging innerHTML and outerHTML

--- JavaScript Practice/JS innerHTML outerHTML/script.js
+++ JavaScript Practice/JS innerHTML outerHTML/script.js
@@ -1,11 +1,13 @@
 let variable = document.body.firstChild.nodeName;
 console.log(variable); // #text
 
-let x = document.getElementsByTagName("span")[0].innerHTML; 
+let x = document.getElementsByTagName("span")[0].innerHTML;
 console.log(x); // Hy I am a Span
 
-let y = document.getElementsByTagName("span")[0].outerHTML; 
+let y = document.getElementsByTagName("span")[0].outerHTML; // outerHTML gives the whole element including the tags
+let first = document.getElementById("first");
+
 console.log(y); // <span>Hy I am a Span</span>
-console.log(first.innerHTML);
-console.log(first.outerHTML);
-console.log(document.body.firstChild.nodeValue)
+console.log(first.innerHTML); // Hy I am a Span
+console.log(first.outerHTML); // <span>Hy I am a Span</span>
+console.log(document.body.firstChild.nodeValue); // hello
