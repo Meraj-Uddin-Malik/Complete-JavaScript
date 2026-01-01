3b9460efc357d430cb60bf9fd809bbdbb9a66c2a
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 01:22:19 +0500

    Add initial HTML structure and JavaScript for innerHTML and outerHTML examples

---
+++ JavaScript Practice/JS innerHTML outerHTML/script.js
@@ -0,0 +1,8 @@
+let variable = document.body.firstChild.nodeName;
+console.log(variable); // #text
+
+let x = document.getElementsByTagName("span")[0].innerHTML; 
+console.log(x); // Hy I am a Span
+
+let y = document.getElementsByTagName("span")[0].outerHTML; 
+console.log(y); // <span>Hy I am a Span</span>
