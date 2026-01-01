9bef0651739b5bc9ce3171bd5f1ce2ddf0e7c617
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 02:57:37 +0500

    Remove unused insertAdjacentHTML line from script.js

--- JavaScript Practice/HTML Insertion Methods/script.js
+++ JavaScript Practice/HTML Insertion Methods/script.js
@@ -5,4 +5,3 @@ let b = document.createElement("div");
 b.innerHTML = "<h1>This is New Element</h1><p>This is new paragraph</p>";
 document.body.appendChild(b);
 b.prepend("This is prepended text.");
-b.insertAdjacentHTML("afterend", "<h2>This is inserted using insertAdjacentHTML</h2>");
