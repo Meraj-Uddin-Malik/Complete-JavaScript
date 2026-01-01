299cae6190d07e4ae02ab0b93efe7b6d28b9c5d3
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 02:46:53 +0500

    Add text prepend functionality to newly created div element

--- JavaScript Practice/HTML Insertion Methods/script.js
+++ JavaScript Practice/HTML Insertion Methods/script.js
@@ -4,3 +4,5 @@ a.innerHTML = a.innerHTML + "<h1>This is heading</h1><p>This is paragraph</p>";
 let b = document.createElement("div");
 b.innerHTML = "<h1>This is New Element</h1><p>This is new paragraph</p>";
 document.body.appendChild(b);
+b.prepend("This is prepended text.");
+b.insertAdjacentHTML("afterend", "<h2>This is inserted using insertAdjacentHTML</h2>");
