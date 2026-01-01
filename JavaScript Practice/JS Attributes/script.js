821039998e7506f9d578ee49d66dbb9a81000b50
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-02 01:49:19 +0500

    Add initial HTML and JavaScript files for attribute manipulation examples

---
+++ JavaScript Practice/JS Attributes/script.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("Begen");
+let first = element.getAttribute("class");
+console.log(first);
+let second = element.getAttribute("id");
+console.log(second);
+let third = element.getAttribute("style");
+// element.setAttribute("hidden", "true");
