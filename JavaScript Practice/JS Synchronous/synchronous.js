845bffb8847b61987ae6180fd90e39f0b4ddb16b
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-03 07:24:15 +0500

    Refactor synchronous.js to improve task function structure and enhance readability

--- JavaScript Practice/JS Synchronous/synchronous.js
+++ JavaScript Practice/JS Synchronous/synchronous.js
@@ -5,23 +5,17 @@ console.log(
 function task1() {
   console.log("Task 1: Starting");
     // Simulating a time-consuming task with a loop
-    for (let i = 0; i < 1e8; i++) {} // Busy-wait loop to simulate delay
-    console.log("Task 1: Completed");
-}
-function task2() {
-    console.log("Task 2: Starting");
-    // Simulating a time-consuming task with a loop
     for (let i = 0; i < 1e8; i++) {}
-    console.log("Task 2: Completed");
+  console.log("Task 1: Completed");
 }
-function task3() {
-    console.log("Task 3: Starting");
-    // Simulating a time-consuming task with a loop
-    for (let i = 0; i < 1e8; i++) {}
-    console.log("Task 3: Completed");
-}
-task1(); // Calling task1
-task2();
-task3();
 
+function task2() {
+  console.log("Task 2: Starting");
+    // Simulating a time-consuming task with a loop
+    for (let i = 0; i < 1e8; i++) {}
+  console.log("Task 2: Completed");
+}
+
+task1(); // Calling task1
+task2(); // Calling task2 after task1 is completed
 console.log("All tasks completed in sequence.");
