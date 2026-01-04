dc2f064e3d1a06ea09cf5732e98353a60ad3158e
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-04 19:56:50 +0500

    Fix formatting in setTimeOut.js by correcting indentation and ensuring consistent code structure

--- JavaScript Practice/JS SetTimeOut/setTimeOut.js
+++ JavaScript Practice/JS SetTimeOut/setTimeOut.js
@@ -3,6 +3,15 @@ console.log(`SetTimeOut is used to delay code for some time period.`);
 console.log("first");
 
 setTimeout(() => {
-    console.log("second");
-}, 4000) ;
+  console.log("second");
+}, 4000);
 console.log("third");
+
+function getData(dataId) {
+  setTimeout(() => {
+    console.log("data", dataId);
+  }, 2000);
+}
+
+getData(5);
+console.log("fetching data...");
