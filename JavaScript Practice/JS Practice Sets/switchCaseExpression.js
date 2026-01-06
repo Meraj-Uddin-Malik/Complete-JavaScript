cd3ed379ff6f767d113dd396ae996f6c038b6fcf
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-06 22:47:20 +0500

    Add switchCaseExpression.js to evaluate voting eligibility based on age

---
+++ JavaScript Practice/JS Practice Sets/switchCaseExpression.js
@@ -0,0 +1,24 @@
+// Q1
+let age = 20;
+if (age > 18 && age < 20) {
+  console.log("you are eligible for voting");
+} else {
+  console.log("Your age is not valid for voting");
+}
+
+
+// Q2
+let age1 = 25;;
+switch (age1) {
+    case 12:
+        console.log("You are 12 years old");
+        break;
+    case 13:
+        console.log("You are 18 years old");
+        break;
+    case 20:
+        console.log("You are 20 years old");
+        break;
+    default:
+        console.log("Your age is not listed");
+}
