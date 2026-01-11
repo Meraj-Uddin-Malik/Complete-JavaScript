0a0ce30930968eab20dbeb4bcdcd57a9a5866218
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-11 23:44:33 +0500

    Add comment to clarify the concept of Callback Hell

--- JavaScript Practice/JS Callback/callbackHell.js
+++ JavaScript Practice/JS Callback/callbackHell.js
@@ -1,3 +1,11 @@
 console.log(
   "a situation where multiple nested callbacks make code difficult to read and maintain."
 );
+
+// Example of Callback Hell
+function fetchData(callback) {
+  setTimeout(() => {
+    console.log("Data fetched");
+    callback("Fetched Data");
+  }, 1000);
+}
