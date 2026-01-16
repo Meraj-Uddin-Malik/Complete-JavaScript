f855fd36a11cc960e54efbdcfb7b59efcf30a20d
Meraj Uddin Malik <141-2021@lms.indus.edu.pk>
2026-01-16 22:35:17 +0500

    Enhance synchronous.js with sync programming details
    Added explanation and example of synchronous programming.

--- JavaScript Practice/JS Synchronous/synchronous.js
+++ JavaScript Practice/JS Synchronous/synchronous.js
@@ -2,6 +2,16 @@ console.log(
   `in Synchronous Programming, the code waits for the previous task to complete`
 );
 
+
+console.log(`1. Synchronous (Sync)
+Meaning: Code runs line by line, one after the other.
+Behavior: Each step waits for the previous step to finish before executing.`);
+// Example
+console.log("Step 1");
+console.log("Step 2");
+console.log("Step 3");
+
+
 function task1() {
   console.log("Task 1: Starting");
     // Simulating a time-consuming task with a loop
