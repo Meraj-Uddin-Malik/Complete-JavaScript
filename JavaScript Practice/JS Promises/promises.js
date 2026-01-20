19f75e651f018a9f118a2ca383894e11f940c4a4
Meraj-Uddin-Malik <merajuddinmalik136@gmail.com>
2026-01-21 00:02:11 +0500

    Update success condition in promises.js to test rejection case

--- JavaScript Practice/JS Promises/promises.js
+++ JavaScript Practice/JS Promises/promises.js
@@ -2,7 +2,7 @@ console.log("Promises In JS is a way to handle asynchronous operations.");
 
 // Creating a new Promise
 const myPromise = new Promise((resolve, reject) => {
-  const success = true; // Change this to false to test rejection case
+  const success = false; // Change this to false to test rejection case
   setTimeout(() => {
     if (success) {
       resolve("Promise resolved successfully!");
