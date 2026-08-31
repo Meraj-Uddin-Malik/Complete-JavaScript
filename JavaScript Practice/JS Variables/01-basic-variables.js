38e0aa6bd11cf660d72368ed3b0e3962589c3b9c
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2026-08-31 20:29:51 +0500

    Update variable examples in 01-basic-variables.js and add new file for strings and numbers

--- JavaScript Practice/JS Variables/01-basic-variables.js
+++ JavaScript Practice/JS Variables/01-basic-variables.js
@@ -1,7 +1,27 @@
-var my_name = "Meraj";
-let age = 25;
-const country = "Pakistan";
+// Q1
+// var my_name = "Meraj";
+// let age = 26;
+// const country = "Pakistan";
 
-console.log(my_name);
-console.log(age);
-console.log(country); 
+// console.log(my_name);
+// console.log(age);
+// console.log(country);
+
+// console.log(typeof my_name);
+// console.log(typeof age);
+// console.log(typeof country);
+
+// Q2
+// let score = 10;
+// console.log(score);
+// score = 20;
+// console.log(score);
+
+// Q1
+var my_name = "Meraj";
+let age = 26;
+const favoriteProgrammingLanguage = "Java";
+
+console.log(typeof my_name);
+console.log(typeof age);
+console.log(typeof favoriteProgrammingLanguage);
