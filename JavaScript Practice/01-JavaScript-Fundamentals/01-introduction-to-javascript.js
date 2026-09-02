bd98055edad58117a5383ee2f9eb69f61fd9c7ba
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2026-09-02 17:45:22 +0500

    Add introductory content for JavaScript fundamentals with definitions and usage

---
+++ JavaScript Practice/01-JavaScript-Fundamentals/01-introduction-to-javascript.js
@@ -0,0 +1,21 @@
+/*
+Topic: Introduction to JavaScript
+
+Definition:
+What is JavaScript?
+Javascript is a programming language that is commonly used to create interactive effects within web browsers. 
+It allows developers to implement complex features on web pages, such as dynamic content updates, multimedia control, 
+and user interface enhancements.
+
+Why do we use JavaScript?
+We use JavaScript to enhance the functionality and interactivity of web pages. 
+It enables developers to create dynamic and responsive user experiences, manipulate HTML and CSS, 
+handle events, and communicate with servers through APIs. JavaScript is essential for building modern 
+web applications and is widely supported across different browsers and platforms.
+
+Where can JavaScript run?
+
+My Understanding:
+
+Practice:
+*/
