c4d94d247aed9d83224552c70bbd1444b9333008
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2026-09-02 19:04:30 +0500

    Add initial outline for "How JavaScript Works" topic in new file

---
+++ JavaScript Practice/02-how-javascript-works.js
@@ -0,0 +1,24 @@
+/*
+Topic: How JavaScript Works
+
+Definition:
+
+What happens when JavaScript code runs?
+
+What is a JavaScript Engine?
+
+What is parsing?
+
+What is compilation?
+
+What is execution?
+
+What is the Call Stack?
+
+My Understanding:
+
+Questions and Answers:
+
+Practice:
+*/
+
